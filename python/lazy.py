1d6b567969ef436abdee513a23774022a383b7cc
Lirian Su <liriansu@gmail.com>
2017-01-22 10:45:56 +0800

    add lazy.py

---
+++ python/lazy.py
@@ -0,0 +1,11 @@
+def wrap(x):
+    print("wrapping {}".format(x))
+    return x
+
+print("calculating a")
+a = {"a": wrap(42), "b": "universe"}.get("b")
+print("a is {}".format(a))
+
+print("calculating b")
+b = {"a": lambda x: wrap(42), "b": lambda: "universe"}.get("b")()  # lazy
+print("b is {}".format(b))
